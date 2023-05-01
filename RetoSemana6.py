1089076a6866f5bc565ad6b6c57cf63b89e6c60a
Juan David <skylokny@gmai.com>
2023-04-30 20:08:27 -0600

    RetoSemanal

---
+++ RetoSemana6.py
@@ -0,0 +1,18 @@
+password = input("Ingrese una contraseña que inicie con un número: ")
+if not password[0].isdigit():
+    print("La contraseña debe iniciar con un número.")
+    exit()
+
+intentos = 1
+while intentos <= 3:
+    password_reingreso = input("Ingrese la contraseña nuevamente: ")
+    if password_reingreso == password:
+        print("Contraseña confirmada.")
+        break
+    else:
+        print("Las contraseñas no coinciden.")
+        intentos += 1
+
+if intentos > 3:
+    print("Ha excedido el número máximo de intentos.")
+    exit()
